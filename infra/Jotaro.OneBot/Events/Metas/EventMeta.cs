dda46c921848610f1b165580a991bf8d5b8e7d68
hmqgg <mitchx7@outlook.com>
2021-10-13 19:07:59 +0800

    feat(onebot/events): remove meta event Lifecycle

--- infra/Jotaro.OneBot/Events/Metas/EventMeta.cs
+++ infra/Jotaro.OneBot/Events/Metas/EventMeta.cs
@@ -8,12 +8,7 @@ public record EventMeta(string SubType, string Uuid, string SelfId, string Platf
     internal const string EventTypeName = "meta";
 }
 
-public record EventMetaLifecycle
-    (string SubType, string Uuid, string SelfId, string Platform, DateTimeOffset Time) : Event(EventSet.MetaLifecycle,
-        SubType, Uuid, SelfId, Platform, Time);
-
 // TODO: Implement `Status` with `get_status` Action.
-
 public record EventMetaHeartbeat([property: JsonPropertyName("interval")] long Interval,
     [property: JsonPropertyName("status")] long Status, string SubType, string Uuid, string SelfId, string Platform,
     DateTimeOffset Time) : Event(EventSet.MetaHeartbeat, SubType, Uuid, SelfId, Platform, Time);
